dac84a2e7ef5f39a29e489a0b9df4743cb79c3a0
read -r -p "Enter your email? " email <jonathantaaffe@Techelevators-MacBook-Air-92.local>
2018-07-25 14:37:58 -0400

    stuff

--- m3-java-capstone/src/main/java/com/techelevator/npgeek/MainController.java
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/MainController.java
@@ -1,17 +1,31 @@
 package com.techelevator.npgeek;
+import java.awt.List;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techelevator.model.Park;
+import com.techelevator.model.ParkDao;
+import com.techelevator.ssg.model.forum.ForumDao;
+
 @Controller
 public class MainController {
-
+	@Autowired
+	private ParkDao parkDao;
+	
 	@RequestMapping(path= {"/","home"})
 	public String displayHomePage() {
 		return "home";
 	}
 	
 	@RequestMapping(path= {"/park_detail","/park_details"})
-	public String displayParkDetails() {
+	public String displayParkDetails(HttpServletRequest request) {
+//		List<Park> currPark = new List {parkDao.getAllParks()};
+		request.setAttribute("currPark", park);
 		return "park_detail";
 	}
 	
@@ -19,4 +33,5 @@ public class MainController {
 	public String displaySurveysPage() {
 		return "surveys";
 	}
+	
 }
