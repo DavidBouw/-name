1f065a00012293f604dfa65b8bbc9ca17e8b1fa4
read -r -p "Enter your email? " email <jonathantaaffe@Techelevators-MacBook-Air-92.local>
2018-07-25 10:47:01 -0400

    main controller setup

--- m3-java-capstone/src/main/java/com/techelevator/npgeek/MainController.java
+++ m3-java-capstone/src/main/java/com/techelevator/npgeek/MainController.java
@@ -1,5 +1,19 @@
 package com.techelevator.npgeek;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
 
+	@RequestMapping(path= {"/","home"})
+	public String displayHomePage() {
+		return "home";
+	}
+	
+	@RequestMapping("/park_detail")
+	public String displayAlien_age() {
+		return "park_detail";
+	}
+	
+	
 }
